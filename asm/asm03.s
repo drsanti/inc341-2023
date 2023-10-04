36171b2ed23f562f37955a4eb0dbafc09751f03c
drsanti <santi.inc.kmutt@gmail.com>
2023-10-04 14:43:46 +0700

    update asm03

--- asm/asm03.s
+++ asm/asm03.s
@@ -1,4 +1,3 @@
-
 # 1) GitHub
 # 2) Registers
 # 3) Alias Registers
@@ -7,15 +6,20 @@
 # GitHub:
 # https://github.com/drsanti/risc-v-assembly-programming
 
+li a0, 0x123
+li a1, 0x124
+li a2, 0x125
+li a3, 0x126
+mv a5, a0
 
 # Registers
 #|----------|-------------------------------------------|
 #|	PC 		|	Program Counter							|
 #|	PRIV 	|	Privilege Levels						|
-#|	      	|	 00: User/Application (U)				|	U: User
-#|	      	|	 01: User/Application (S)				|	S: Supervisor
+#|	      	|	 00: User/Application 	(U)				|	U: User
+#|	      	|	 01: Supervisor 		(S)				|	S: Supervisor
 #|	      	|	 10: -									|	M: Machine
-#|	      	|	 11: User/Application (M)				|
+#|	      	|	 11: Machine 			(M)				|
 #|	CSR 	|	Control and Status Registers			|
 #|	Integer	|	Standard RISC-V registers x00-x31		|
 #|	Float	|	Floating-pointregisters f00-f31 		|
@@ -33,9 +37,3 @@
 #|	s0..11 	|	Saved register							|
 #|	a0..7 	|	Function arguments/return values		|
 #|----------|-------------------------------------------|
-
-
-
-
-
-

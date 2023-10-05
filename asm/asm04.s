8a19b75e8bac77dadf6c6d41af62bc200c12b336
drsanti <santi.inc.kmutt@gmail.com>
2023-10-05 09:40:57 +0700

    update asm04.asm

--- asm/asm04.s
+++ asm/asm04.s
@@ -1,5 +1,17 @@
-# 1) Load an immediate (constant) value into a register
-# 2) Register-to-register data transfer
+# li: Load an immediate (constant) value.
+li a0, 12         	# Load immediate 12 into register a0
+li a1, 0x12AB     	# Load immediate 0x12AB into register a1
+li a2, 0b11001111 	# Load immediate binary value 11001111 into register a2
+nop              	# No operation (placeholder)
+
+
+
+# mv: Move/Copy (Register-to-Register)
+li a1, 0x123ABC   	# Load immediate 0x123ABC into register a1
+mv a0, a1         	# Move the value of a1 into a0
+nop              	# No operation (placeholder)
+
+
 
 # Load an immediate (constant) value into a register.
 #|--------------------------|
@@ -18,5 +30,3 @@
 # mv: Move
 # rd: Destination register where the value from register rs will be copied.
 # rs: Source register from which the value will be copied to rd.
-
-
